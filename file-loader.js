f8cf4548b0754b3a80a4f469b49bddf8ce898192
Spencer Alan Watson <spenceralanwatson@gmail.com>
2014-11-19 10:54:04 -0600

    Created the base of the file loader file.

---
+++ file-loader.js
@@ -0,0 +1,12 @@
+;
+(function (global, undefined) {
+    'use strict';
+    function FileLoader() {
+        var loader = {};
+        
+        return loader;
+    };
+    
+    global.Game = global.Game || {};
+    global.Game.FileLoader = FileLoader;
+});
