2058bd3add7d33874ac184cf325d5c81720deead
Spencer Alan Watson <spenceralanwatson@gmail.com>
2014-11-19 12:08:00 -0600

    Added load and sendRequest functions
    #FileLoader.load
    
    | Parameters     | Description       |
    | :—————— |:———————|
    | type | general type of file, so js for js and img for images |
    | name | The name of the file (with extension) and path if type is not
    predefined        |
    | callback | a function that will be called when the file is received
    with the parameters err, and data|

--- file-loader.js
+++ file-loader.js
@@ -1,12 +1,45 @@
 ;
 (function (global, undefined) {
     'use strict';
+
     function FileLoader() {
-        var loader = {};
-        
+        var loader = {
+            dirs: {
+                'js': 'js/'
+            }
+        };
+
+        loader.sendRequest = function (filePath, callback) {
+            //We do not support IE6 or below, so no need for ActiveXObject
+            var req = new XMLHttpRequest();
+            try {
+                req.addEventListener('readystatechange', function (event) {
+                    if (req.readyState === 4) {
+                        if (req.status === 200) {
+                            callback(null, req.responseText);
+                        } else {
+                            callback(req, req.responseText);
+                        }
+                    }
+                });
+                req.open("GET", filePath, true);
+                req.send();
+            } catch (err) {
+                callback(err, req);
+            }
+
+        };
+
+
+        loader.load = function (type, name, callback) {
+            var dir = loader.dirs[type] ? loader.dirs[type] + name : name;
+
+            loader.sendRequest(dir, callback);
+        };
+
         return loader;
     };
-    
+
     global.Game = global.Game || {};
     global.Game.FileLoader = FileLoader;
-});
+}(this));
