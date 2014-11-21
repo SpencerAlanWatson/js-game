de427fa46b4a44088c3b0c3539b631780cb2f14c
Spencer Alan Watson <spenceralanwatson@gmail.com>
2014-11-21 11:33:02 -0600

    Changing file-loader.js to use promises

--- js/file-loader.js
+++ js/file-loader.js
@@ -14,27 +14,32 @@
             }
         };
 
-        loader.sendRequest = function (filePath, callback) {
-            //We do not support IE6 or below, so no need for ActiveXObject
-            var req = new XMLHttpRequest();
-
-            req.addEventListener('readystatechange', function (event) {
-                if (req.readyState === 4) {
-                    if (req.status === 200)
-                        callback(null, req);
-                    else
-                        callback(req.status, req);
-                }
+        loader.sendRequest = function (filePath) {
+            return new Promise(function (resolve, reject) {
+                //We do not support IE6 or below, so no need for ActiveXObject
+                var req = new XMLHttpRequest();
+                req.addEventListener('readystatechange', function (event) {
+                    if (req.readyState === 4) {
+                        if (req.status === 200)
+                            resolve(req); //callback(null, req);
+                        else
+                            reject(req);
+                    }
+                });
+                req.open("GET", filePath, true);
+                req.send();
             });
-            req.open("GET", filePath, true);
-            req.send();
+
         };
+
         loader.sendRequestSync = function (filePath) {
             //We do not support IE6 or below, so no need for ActiveXObject
+
             var req = new XMLHttpRequest();
             req.open("GET", filePath, false);
             req.send();
             return req;
+
         };
 
         loader.getFilePaths = function (type, names) {
@@ -48,42 +53,58 @@
             }
         };
 
-        function loadFileFactory(filesToLoad, callback) {
-            var filesLoaded = 0,
-                errCount = 0,
-                results = {
-                    files: {}
-                };
-            return function (err, data, name) {
+        function loadFileFactory(filesToLoad, resolve, reject) {
+            var results = {
+                files: _.create(null),
+                errCount: 0,
+                filesLoaded: 0
+            };
 
-                ++filesLoaded;
 
-                if (err) {
-                    ++errCount;
-                    if (!results.errors) results.errors = {};
-                    results.errors[name] = data;
-                } else {
+            return [
+
+                function (data, name) {
+
+                    ++results.filesLoaded;
                     results.files[name] = data.responseText;
-                }
 
 
-                if (filesLoaded === filesToLoad) {
-                    callback(errCount, results);
-                }
+                    if (results.filesLoaded === filesToLoad) {
+                        resolve(results);
+                    }
 
-            }
+            },
+                function (error, name) {
+                    ++results.filesLoaded;
+                    ++results.errCount;
+                    if (!results.errors) results.errors = _.create(null);
+                    results.errors[name] = error;
+
+
+                    if (results.filesLoaded === filesToLoad && results.errCount === filesToLoad) {
+                        reject(results);
+                    } else if (results.filesLoaded === filesToLoad) {
+                        resolve(results);
+                    }
+            }];
         }
         loader.load = function (type, fileNames, callback) {
 
             //If it is a string, this will make it an array,
             //if it is an array, it will change nothing.
-            var names = [].concat(fileNames),
-                dirs = loader.getFilePaths(type, names),
-                onFileLoad = loadFileFactory(dirs.length, callback);
 
-            _.each(dirs, function (dir, index) {
-                loader.sendRequest(dir, _.partialRight(onFileLoad, names[index]));
+            return new Promise(function (resolve, reject) {
+                var names = [].concat(fileNames),
+                    dirs = loader.getFilePaths(type, names),
+                    onFileLoad = loadFileFactory(dirs.length, resolve, reject);
+                _.each(dirs, function (dir, index) {
+                    let name = names[index];
+                    loader.sendRequest(dir)
+                        .then(_.partialRight(onFileLoad[0], name),
+                            _.partialRight(onFileLoad[1], name));
+                });
             });
+
         };
 
         loader.loadSync = function (type, fileNames) {
@@ -93,19 +114,22 @@
             var names = [].concat(fileNames),
                 dirs = loader.getFilePaths(type, names),
                 results = {
-                    files: {}
+                    files: _.create(null),
+                    errCount: 0,
+                    filesLoaded: 0
                 };
 
             _.each(dirs, function (dir, index) {
                 var req = loader.sendRequestSync(dir),
                     name = names[index];
+                ++results.filesLoaded;
                 if (req.status !== 200) {
-                    if(!results.errors) results.errors = {};
+                    if (!results.errors) results.errors = _.create(null);
+                    ++results.errCount;
                     results.errors[name] = req;
                 } else {
                     results.files[name] = req.response;
                 }
-                //loader.sendRequest(dir, _.partialRight(onFileLoad, names[index]));
             });
             return results;
         };
